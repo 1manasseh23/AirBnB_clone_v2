f0d433345ba27cf00d52ee15fb313cf561e90820
1manasseh23 <cletavivan@gmail.com>
2024-04-20 18:03:05 +0000

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-
+# This is to import Flask
 from flask import Flask
 
+
+# Create a Flask app instance
 app = Flask(__name__)
 
+# Define a route for the root URL ('/')
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    This function handles requests to the root URL ('/').
+    When a user accesses the root URL, it returns the string 'Hello HBNB!'.
+    """
     return 'Hello HBNB!'
 
+# Run the app if this script is executed directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
