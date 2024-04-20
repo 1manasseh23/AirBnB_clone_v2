63d570ddb23e303722545f92ffd04e78f267e571
1manasseh23 <cletavivan@gmail.com>
2024-04-20 19:53:25 +0000

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,16 +17,18 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
-    """
-    This function handles requests to the root URL ('/').
-    When a user accesses the root URL, it returns the string 'Hello HBNB!'.
-    """
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     return "C " + text.replace("_", " ")
 
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    return "Python " + text.replace("_", " ")
+
 # Run the app if this script is executed directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
