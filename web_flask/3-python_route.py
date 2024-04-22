66c4721ee987ca58810794d4ad83512629fe2731
1manasseh23 <cletavivan@gmail.com>
2024-04-22 20:57:42 +0100

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-# Import the Flask module
+"""Import the Flask module"""
 from flask import Flask
 
-# Create a Flask app instance
 app = Flask(__name__)
 
 
-# Define a route for the root URL ('/')
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    This function handles requests to the root URL ('/').
-    When a user accesses the root URL, it returns the string 'Hello HBNB!'.
-    """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     return "C " + text.replace("_", " ")
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text):
     return "Python " + text.replace("_", " ")
 
-# Run the app if this script is executed directly
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
